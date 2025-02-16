1fc881680b4767fa6114cbf3c6659d79bda99d6c
johnkatua <johnkatua99@gmail.com>
2025-02-17 01:17:47 +0300

    add global styles and enhance Home component layout with background image

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,24 @@
+import Image from "next/image";
+
 export default function Home() {
   return (
-    <div className="bg-slate-400">
-      <div>One</div>
-      <div>Two</div>
+    <div className="h-screen">
+      <div className="h-full relative">
+        <Image
+          src={"/white-bg.jpg"}
+          alt="White-bg"
+          width={0}
+          height={0}
+          className="object-fill w-full h-full"
+        />
+        <div className="absolute top-0 left-0 bottom-0 right-0 p-6">
+          <div className="bg-slate-300 flex items-center justify-center">
+            <div className="bg-black text-white py-2 px-4 rounded-3xl w-[560px]">
+              Header
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
