80217e778bd26de793696d691fe9d2cf03c0df87
johnkatua <johnkatua99@gmail.com>
2025-02-17 00:48:51 +0300

    update Home component layout and add LoginPage component

---
+++ app/login/page.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const LoginPage = () => {
+  return <div>LoginPage</div>;
+};
+
+export default LoginPage;
+
+// https://unsplash.com/photos/white-and-gray-optical-illusion-7JX0-bfiuxQ?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash
--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,8 @@
 export default function Home() {
-  return <div>hello</div>;
+  return (
+    <div className="bg-slate-400">
+      <div>One</div>
+      <div>Two</div>
+    </div>
+  );
 }
