7e8ea05e3b48518cba7af206d022ba2e12e4356b
johnkatua <johnkatua99@gmail.com>
2025-02-17 12:19:48 +0300

    refactor MGDialog component to utilize AlertDialog from Radix UI for enhanced dialog functionality

--- app/components/mg-dialog/index.tsx
+++ app/components/mg-dialog/index.tsx
@@ -1,8 +1,44 @@
-import React from "react";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+} from "@/components/ui/alert-dialog";
+import { FC, ReactNode } from "react";
+
+interface MGDialogProps {
+  btnText: String;
+  description: String;
+  title: String;
+  children?: ReactNode;
+}
 
 // MGDialog -> A custom Mafia Game Dialog
-const MGDialog = () => {
-  return <div>MGDialog</div>;
+const MGDialog: FC<MGDialogProps> = ({
+  btnText,
+  description,
+  title,
+  children,
+}) => {
+  return (
+    <AlertDialog>
+      <AlertDialogContent>
+        <AlertDialogHeader>
+          <AlertDialogTitle>{title}</AlertDialogTitle>
+          <AlertDialogDescription>{description}</AlertDialogDescription>
+        </AlertDialogHeader>
+        {children}
+        <AlertDialogFooter>
+          <AlertDialogCancel>Cancel</AlertDialogCancel>
+          <AlertDialogAction>{btnText}</AlertDialogAction>
+        </AlertDialogFooter>
+      </AlertDialogContent>
+    </AlertDialog>
+  );
 };
 
 export default MGDialog;
