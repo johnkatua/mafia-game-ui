35e5371ec77b8f2e3e6d6698c735c5ebb940c941
johnkatua <johnkatua99@gmail.com>
2025-02-17 11:33:03 +0300

    remove unused LoginPage component

--- app/login/page.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-const LoginPage = () => {
-  return <div>LoginPage</div>;
-};
-
-export default LoginPage;
-
-// https://unsplash.com/photos/white-and-gray-optical-illusion-7JX0-bfiuxQ?utm_content=creditShareLink&utm_medium=referral&utm_source=unsplash
