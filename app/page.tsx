7f428916bc470f90a3be5a41198942c4bac4a660
johnkatua <johnkatua99@gmail.com>
2025-02-17 16:16:13 +0300

    refactor Home component to remove commented code and reorganize imports

--- app/page.tsx
+++ app/page.tsx
@@ -1,35 +1,9 @@
-// import Image from "next/image";
-
-// export default function Home() {
-//   return (
-//     <div className="h-screen">
-//       <div className="h-full relative">
-//         <Image
-//           src={"/white-bg.jpg"}
-//           alt="White-bg"
-//           width={0}
-//           height={0}
-//           className="object-fill w-full h-full"
-//         />
-//         <div className="absolute top-0 left-0 bottom-0 right-0 p-6">
-//           <div className="bg-slate-300 flex items-center justify-center">
-//             <div className="bg-header-color text-white py-2 px-4 rounded-3xl w-[560px]">
-//               Header
-//             </div>
-//           </div>
-//         </div>
-//       </div>
-//     </div>
-//   );
-// }
-
-import { Button } from "@/components/ui/button";
+import CTA from "./components/CTA";
 import FeaturesSection from "./components/FeaturesSection";
+import HeroSection from "./components/HeroSection";
 import HowItWorks from "./components/HowItWorks";
 import LiveStats from "./components/LiveStats";
 import Testimonials from "./components/Testimonials";
-import CTA from "./components/CTA";
-import HeroSection from "./components/HeroSection";
 
 export default function Home() {
   return (
