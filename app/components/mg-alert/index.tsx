c812d9b84b1de12fe4d1de73f49ecf0497b4ca10
johnkatua <johnkatua99@gmail.com>
2025-02-18 11:41:25 +0300

    enhance MGAlert component with animation for improved visibility

--- app/components/mg-alert/index.tsx
+++ app/components/mg-alert/index.tsx
@@ -1,12 +1,19 @@
 import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
 import { Terminal } from "lucide-react";
 import React from "react";
+import { motion } from "framer-motion";
 
 // MGAlert -> A custom Mafia Game Alert Component
 const MGAlert = () => {
   return (
-    <Alert>
-      <Terminal className="h-4 w-4" />
+    <Alert className="fixed left-2 bottom-2">
+      <motion.div
+        initial={{ opacity: 0 }}
+        animate={{ opacity: 1 }}
+        transition={{ ease: "linear", duration: 0.5, repeat: Infinity }}
+      >
+        <Terminal className="h-4 w-4" />
+      </motion.div>
       <AlertTitle>Heads up!</AlertTitle>
       <AlertDescription>This is an Alert</AlertDescription>
     </Alert>
