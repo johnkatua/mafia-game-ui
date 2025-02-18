5151c222c91804b6a1768e1564d9e42b4cc3c6a2
johnkatua <johnkatua99@gmail.com>
2025-02-18 11:34:03 +0300

    enhance MGAlert component with title and description for better user feedback

--- app/components/mg-alert/index.tsx
+++ app/components/mg-alert/index.tsx
@@ -1,11 +1,14 @@
-import { Alert } from "@/components/ui/alert";
+import { Alert, AlertDescription, AlertTitle } from "@/components/ui/alert";
+import { Terminal } from "lucide-react";
 import React from "react";
 
 // MGAlert -> A custom Mafia Game Alert Component
 const MGAlert = () => {
   return (
     <Alert>
-      <div>MGAlert</div>
+      <Terminal className="h-4 w-4" />
+      <AlertTitle>Heads up!</AlertTitle>
+      <AlertDescription>This is an Alert</AlertDescription>
     </Alert>
   );
 };
