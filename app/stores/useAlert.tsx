1b471c08bcd5dc71663eb75c13b45e120d081dc2
johnkatua <johnkatua99@gmail.com>
2025-02-18 13:21:10 +0300

    update useAlertStore to manage alert data with openAlert and closeAlert actions

--- app/stores/useAlert.tsx
+++ app/stores/useAlert.tsx
@@ -1,16 +1,24 @@
 import { create } from "zustand";
 
+type AlertData = {
+  game_id: string;
+  join: string;
+  host: string;
+};
+
 type State = {
   isOpen: boolean;
+  data: AlertData | null;
 };
 
 type Action = {
-  openAlert: () => void;
+  openAlert: (data: AlertData) => void;
   closeAlert: () => void;
 };
 
 export const useAlertStore = create<State & Action>((set) => ({
   isOpen: false,
-  openAlert: () => set({ isOpen: true }),
-  closeAlert: () => set({ isOpen: false }),
+  data: null,
+  openAlert: (data) => set({ isOpen: true, data }),
+  closeAlert: () => set({ isOpen: false, data: null }),
 }));
