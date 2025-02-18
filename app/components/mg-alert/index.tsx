5795f18b5bc2fd561f1a4eb4dbe0dff2f0c6e346
johnkatua <johnkatua99@gmail.com>
2025-02-18 11:31:13 +0300

    add MGAlert component as a custom alert for the Mafia Game

---
+++ app/components/mg-alert/index.tsx
@@ -0,0 +1,13 @@
+import { Alert } from "@/components/ui/alert";
+import React from "react";
+
+// MGAlert -> A custom Mafia Game Alert Component
+const MGAlert = () => {
+  return (
+    <Alert>
+      <div>MGAlert</div>
+    </Alert>
+  );
+};
+
+export default MGAlert;
